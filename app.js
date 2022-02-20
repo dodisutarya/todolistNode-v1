a37d9d7834286c8fa9f3f272be0a25b833ba6f91
Dodi Sutarya <9284479+dodisutarya@users.noreply.github.com>
2022-02-20 10:22:44 +0700

    this code is done

--- app.js
+++ app.js
@@ -2,80 +2,60 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
+
 
 const app = express();
 
-var item = [];
+const items = ["Buy Food", "Cook Food", "Eat Food"];
+const workItems = [];
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
 
 app.get("/", function (req, res) {
 
-    var today = new Date();
-    var options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long"
-    };
+    const day = date.getDate();
 
-    var day = today.toLocaleDateString("en-US", options);
-
-    // var currentDay = today.getDay();
-    // var day = "";
-
-    // Checking Weekend/Weekday
-    // if (currentDay === 6 || currentDay === 0) {
-    //     day = "weekend";
-    // } else {
-    //     day = "weekday";
-    // }
-
-    // Checking Name of The Day
-    // switch (currentDay) {
-    //     case 0:
-    //         day = "Minggu";
-    //         break;
-    //     case 1:
-    //         day = "Senin";
-    //         break;
-    //     case 2:
-    //         day = "Selasa";
-    //         break;
-    //     case 3:
-    //         day = "Rabu";
-    //         break;
-    //     case 4:
-    //         day = "Kamis";
-    //         break;
-    //     case 5:
-    //         day = "Jumat";
-    //         break;
-    //     case 6:
-    //         day = "Sabtu";
-    //         break;
-    //     default:
-    //         console.log("Error: current day is equal to : " + currentDay);
-    // }
-
-
-
-    res.render("list", { 
-        kindOfDay: day,
-        newListItems: items 
+    res.render("list", {
+        listTitle: day,
+        newListItems: items
     });
 
 });
 
-app.post("/", function(req, res){
-   var item = req.body.newItem;
+app.post("/", function (req, res) {
 
-   items.push(item);
-   
-   res.redirect("/");
+    console.log(req.body.list);
+
+    const item = req.body.newItem;
+
+    if (req.body.list === "Work") {
+        workItems.push(item);
+        res.redirect("/work");
+    } else {
+        items.push(item);
+        res.redirect("/");
+    }
 
 });
+
+app.get("/work", function (req, res) {
+    res.render("list", { listTitle: "Work List", newListItems: workItems });
+});
+
+app.post("/work", function (req, res) {
+    let item = req.body.newItem;
+    workItems.push(item);
+    res.redirect("/work");
+});
+
+app.get("/about", function(req, res){
+    res.render("about");
+})
 
 app.listen(3000, function () {
     console.log("Server berjalan di port 3000");
